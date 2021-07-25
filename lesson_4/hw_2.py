74f693062bf2058e64544e1c81dd659b5b96ce41
gleb_gury <faiter8@mail.ru>
2021-07-25 15:00:33 +0300

    lesson_4

--- lesson_4/hw_2.py
+++ lesson_4/hw_2.py
@@ -53,3 +53,31 @@ if __name__ == '__main__':
     cProfile.run('main()')
 
 # Вывод: "Алгоритм без решето срабатывает быстее за счет того что останавливает генерацию на i-ом элементе"
+'''
+"C:\Program Files\Python39\python.exe" C:/pythonProject19_07_2021/lesson_4/hw_2.py
+Укажите порядковый номер простого числа: 1000
+Используя алгоритм «Решето Эратосфена» i = 7919
+Не используя алгоритм «Решето Эратосфена» i = 7919
+         665593 function calls in 9.388 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    9.388    9.388 <string>:1(<module>)
+        1    0.000    0.000    0.000    0.000 codecs.py:319(decode)
+        1    0.000    0.000    0.000    0.000 codecs.py:331(getstate)
+        1    0.029    0.029    0.029    0.029 hw_2.py:29(i_nat_numb_not_ert)
+        1    0.098    0.098    9.388    9.388 hw_2.py:45(main)
+        1    4.890    4.890    5.540    5.540 hw_2.py:7(i_nat_numb_ert)
+        1    0.580    0.580    0.580    0.580 hw_2.py:8(<listcomp>)
+        1    0.000    0.000    0.000    0.000 {built-in method _codecs.utf_8_decode}
+        1    0.000    0.000    9.388    9.388 {built-in method builtins.exec}
+        1    3.721    3.721    3.721    3.721 {built-in method builtins.input}
+        2    0.000    0.000    0.000    0.000 {built-in method builtins.print}
+   665580    0.070    0.000    0.070    0.000 {method 'append' of 'list' objects}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+
+
+Process finished with exit code 0
+'''
